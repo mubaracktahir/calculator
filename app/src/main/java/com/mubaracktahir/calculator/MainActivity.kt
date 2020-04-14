e18546e7e44c7ca0838ffbc4f93b5c189ee4e6c1
Mubarack tahir <mubarack.tahir@yahoo.com>
2020-04-14 13:25:07 +0100

    added listeners to all the buttons

--- app/src/main/java/com/mubaracktahir/calculator/MainActivity.kt
+++ app/src/main/java/com/mubaracktahir/calculator/MainActivity.kt
@@ -5,15 +5,32 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
 
+    override fun onClick(p0: View?) {
+        when(p0?.id){
+            R.id.equalto ->{
 
+            }
+            R.id.divide ->{
 
+            }
+            R.id.multiply ->{
+
+            }
+            R.id.add ->{
+
+            }
+            R.id.subtract ->{
+
+            }
+        }
+    }
 
 
 }
